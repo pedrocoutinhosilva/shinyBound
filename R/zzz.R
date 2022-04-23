1d229b9d33d06a97b7732745ef95904e840bdc86
Pedro Silva <pedro@appsilon.com>
2022-04-23 21:34:33 +0200

    feat: Scaffold package.

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+
+# Initializes all package options on package loading
+.onLoad <- function(libname, pkgname) {
+  # Initializes global option list for keeping registered components
+  options(shinyhtml.components = list())
+}
